20889525b957ca728f3da7bc18fa69346ad82a20
brantnej <brantnej@oregonstate.edu>
2022-04-28 17:02:49 -0700

    added starter code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require('express-handlebars')
 const fs = require('fs')
 var app = express()
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.json())
@@ -15,10 +15,17 @@ app.use('application/javascript', express.static(__dirname + '/public/index.js')
 var port = 28828
 
 app.get('/', (req, res, next) => {
-    var Context = {'tables' : []}
+    var Context = {'tables' : [
+      {"table": "Users"},
+      {"table": "Posts"},
+      {"table": "Comments"},
+      {"table": "Games"},
+      {"table": "Developers"},
+      {"table": "Friendships"},
+      {"table": "GameOwnerships"},
+    ]}
     res.status(200).render('index', Context)
 })
-
 
 app.listen(port, function (err) {
     if (err) {
